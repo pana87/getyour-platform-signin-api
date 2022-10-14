db97d6b7f2d731caf3a640523ef125caab417ee0
pnts87 <p.tsivelekidis@gmail.com>
2022-10-14 13:32:41 +0200

    trying to fix heroku bug

--- src/index.js
+++ src/index.js
@@ -3,22 +3,15 @@ const cors = require('cors')
 const { _getAccountInformationFromHederaMainnetRestApi } = require('./fetch.js')
 
 const app = express()
-const port = 80
 
 app.use(cors())
 app.use(express.json())
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS')
-  next();
-})
 
 app.post('/', async (req, res) => {
   const data = await _getAccountInformationFromHederaMainnetRestApi(req.body.accountId)
   res.send(data)
 })
 
-app.listen(port, () => {
-  console.log(`Signin endpoint listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Signin endpoint listening on port ${process.env.PORT}`)
 })
